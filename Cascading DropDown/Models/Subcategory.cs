415b626afdd65003351acacdf1f093f50290db9a
Arunav <arunav@vonome.com>
2021-04-02 19:37:28 +0600

    Add Subcategory class

---
+++ Cascading DropDown/Models/Subcategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cascading_DropDown.Models
+{
+    public class Subcategory
+    {
+        [Key]
+        public int SubcategoryId { get; set; }
+        public string SubcategoryName { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+    }
+}
